df0d57d35daba81abf264e35203cd6773498e1ef
qihao <qihao@raybattle.com>
2020-04-22 17:24:54 +0800

    移除了无效代码

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from './store'
 import FastClick from 'fastclick'
 import { vuePlugin } from "castle-haozijunqaq-utils";
 import './components/componentRegister'
-import './store'
 import './utils/fixIosInput'
 import './utils/rem'
 import './assets/scss/common.scss'
