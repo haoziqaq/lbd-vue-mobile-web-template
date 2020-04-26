e4312a5e9bd40b7ffb36f01de9948d4b71cab3f4
qihao <qihao@raybattle.com>
2020-04-26 16:08:47 +0800

    add audio.js

---
+++ src/utils/audio.js
@@ -0,0 +1,26 @@
+export function createBGM(src) {
+  let preBGM = document.getElementById('bgm');
+  preBGM && document.body.removeChild(preBGM);
+
+  let audio = document.createElement('audio');
+  audio.src = src;
+  audio.loop = true;
+  audio.preload = true;
+  audio.id = 'bgm';
+
+  document.addEventListener("WeixinJSBridgeReady", () => {
+    audio.play();
+  });
+  document.body.appendChild(audio);
+}
+
+export function playBGM() {
+  let audio = document.getElementById('bgm');
+  audio.play();
+}
+
+export function resetBGM() {
+  let audio = document.getElementById('bgm');
+  audio.currentTime = 0;
+  audio.pause();
+}
