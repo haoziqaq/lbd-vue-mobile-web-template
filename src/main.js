43ab4e98629a365aae0271891f79465442492be9
qihao <qihao@raybattle.com>
2020-06-10 10:16:42 +0800

    add Toast config

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import FastClick from 'fastclick'
 import AlloyFinger from 'alloyfinger'
 import AlloyFingerPlugin from 'alloyfinger/vue/alloy_finger_vue'
 import { vuePlugin } from "castle-haozijunqaq-utils";
+import { Toast } from 'vant'
 import './components/componentRegister'
 import './utils/fixIosInput'
 import './utils/rem'
 import './assets/scss/common.scss'
+
+Toast.setDefaultOptions('loading', { forbidClick: true, duration: 0 });
 
 FastClick.attach(document.body);
 Vue.config.productionTip = false;
