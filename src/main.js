e66293d457799f024b9c4cccadd4eaaea5a49ae0
qihao <qihao@raybattle.com>
2020-10-19 21:59:19 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,18 +4,19 @@ import router from './router'
 import store from './store'
 import VConsole from 'vconsole'
 import { vuePlugin } from "castle-haozijunqaq-utils";
-import { Toast } from 'vant'
+import { Toast, Dialog } from 'vant'
 import './components/componentRegister'
 import './utils/fixWechatBug'
 import './utils/rem'
 import 'normalize.css'
 import './assets/scss/common.scss'
 
-Toast.setDefaultOptions('loading', { forbidClick: true, duration: 0, className: 'lbd-toast-z-index' });
-Toast.setDefaultOptions('text', { className: 'lbd-toast-z-index' });
-Toast.setDefaultOptions('success', { className: 'lbd-toast-z-index' });
-Toast.setDefaultOptions('fail', { className: 'lbd-toast-z-index' });
-Toast.setDefaultOptions('html', { className: 'lbd-toast-z-index' });
+Toast.setDefaultOptions('loading', { forbidClick: true, duration: 0, className: 'lbd-z-index' });
+Toast.setDefaultOptions('text', { className: 'lbd-z-index' });
+Toast.setDefaultOptions('success', { className: 'lbd-z-index' });
+Toast.setDefaultOptions('fail', { className: 'lbd-z-index' });
+Toast.setDefaultOptions('html', { className: 'lbd-z-index' });
+Dialog.setDefaultOptions({ className: 'lbd-z-index' })
 
 Vue.config.productionTip = false;
 
