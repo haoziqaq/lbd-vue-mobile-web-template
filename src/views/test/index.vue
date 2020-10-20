c8fb1dc3b4d1675880e43d650764865059c7179d
qihao <qihao@raybattle.com>
2020-10-20 12:47:19 +0800

    update

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -9,7 +9,7 @@
         :index="0"
         background-color="red"
       >
-        <div class="page animate__animated animate__slideInLeft">背景颜色案例</div>
+        <div class="page animate__animated animate__slideInLeft" @click="showDialog">显示Dialog</div>
       </lbd-vertical-swiper-slide>
       <lbd-vertical-swiper-slide
         :background-image="require('./bg.jpg')"
@@ -37,24 +37,68 @@
         </div>
       </lbd-vertical-swiper-slide>
     </lbd-vertical-swiper>
+
+    <lbd-dialog
+      v-model="isShowDialog"
+      title="声明式弹窗"
+      message="测试声明式弹窗"
+      :show-cancel-button="true"
+      @confirm="handleConfirm"
+      @cancel="handleCancel"
+    />
   </div>
 </template>
 
 <script>
-import { VerticalSwiper, VerticalSwiperSlide } from 'lbd-ui'
+import { VerticalSwiper, VerticalSwiperSlide, Dialog } from 'lbd-ui'
+import { Area } from 'vant';
 import 'animate.css'
 export default {
   name: 'example',
   components: {
     [VerticalSwiper.name]: VerticalSwiper,
-    [VerticalSwiperSlide.name]: VerticalSwiperSlide
+    [VerticalSwiperSlide.name]: VerticalSwiperSlide,
+    [Dialog.name]: Dialog,
   },
   data: () => ({
+    isShowDialog: false,
     noSwipe: false
   }),
   methods: {
     to() {
       this.$refs['swiper'].swiperInstance.slideTo(0, 1000)
+    },
+    showDialog() {
+      this.isShowDialog = true
+      Dialog
+        .confirm('测试confirm')
+        .then(() => {
+          console.log('confirm确认')
+        })
+        .catch(() => {
+          console.log('confirm取消')
+        })
+
+      Dialog
+        .alert({
+          title: '测试alert标题',
+          message: '测试alert内容'
+        })
+        .then(() => {
+          console.log('alert确认')
+        })
+
+      Dialog
+        .alert('测试alert')
+        .then(() => {
+          console.log('alert确认')
+        })
+    },
+    handleConfirm() {
+      console.log('声明式确认')
+    },
+    handleCancel() {
+      console.log('声明式取消')
     }
   }
 }
