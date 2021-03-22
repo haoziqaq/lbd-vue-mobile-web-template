c30b50b03e5c3b935f7cb1566811d09028cdf196
haoziqaq <357229046@qq.com>
2021-03-22 14:36:30 +0800

    1

---
+++ src/views/home/index.vue
@@ -0,0 +1,25 @@
+/**
+ * @fileOverview /home
+ * @author lbd
+ * @date 2021-03-22
+ */
+<template>
+  <div class="main-container"></div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data: () => ({}),
+}
+</script>
+
+<style scoped lang="less">
+.main-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
